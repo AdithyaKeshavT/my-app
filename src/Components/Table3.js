7c298eb98cd7cebe3fa25d3e7daa4b739cb13a07
AdithyaKeshavT <adityakeshav.ak@gmail.com>
2022-01-18 11:38:06 +0530

    commit

--- src/Components/Table3.js
+++ src/Components/Table3.js
@@ -8,19 +8,22 @@ import { Dropdown } from 'primereact/dropdown';
 const Table3 = () => {
     const [data1, setData] = React.useState([
         {
-            id: 0,
             name: "Prakash",
             city: "Istanbul",
             mobile: 9374839945
         },
         {
-            id: 1,
             name: "John",
             city: "Paris",
             mobile: 8497404853
         }
     ]);
-    const [ids, setids] = React.useState(2);
+    const [adding, setadding] = React.useState(false)
+    const [editing, setediting] = React.useState(false)
+    const [editIndex, seteditIndex] = React.useState(null)
+    const [name, setname] = React.useState('')
+    const [city, setcity] = React.useState('')
+    const [mobile, setmobile] = React.useState(undefined)
     const cities = [
         { label: 'New York', value: 'New York' },
         { label: 'Rome', value: 'Rome' },
@@ -28,56 +31,146 @@ const Table3 = () => {
         { label: 'Istanbul', value: 'Istanbul' },
         { label: 'Paris', value: 'Paris' }
     ]
-    const [editingRows, setEditingRows] = React.useState({});
-    const setActiveRowIndex = (index) => {
-        let _editingRows = { ...editingRows, ...{ [`${data1[index].id}`]: true } };
-        setEditingRows(_editingRows);
-    }
-    const onRowEditChange = (e) => {
-        setEditingRows(e.data);
-    }
-    const dropEditor = (s) => {
-        return <Dropdown value={s.value} options={cities} onChange={(e) => s.editorCallback(e.value)} />
-    }
-    const action = (b) => {
+    const action = (obj) => {
+        let index = data1.findIndex(a => a === obj)
+        if (adding && index === data1.length - 1) {
+            return (
+                <>
+                    <Button
+                        label='Add'
+                        onClick={() => {
+                            let cd = [...data1]
+                            cd[data1.length - 1]["name"] = name
+                            cd[data1.length - 1]["city"] = city
+                            cd[data1.length - 1]["mobile"] = mobile
+                            setData(cd)
+                            setadding(false)
+                            setname('')
+                            setcity('')
+                            setmobile(undefined)
+                        }}
+                    />
+                    <Button
+                        label='Cancel'
+                        onClick={() => {
+                            let cd = [...data1]
+                            cd.pop()
+                            setData(cd)
+                            setadding(false)
+                            setname('')
+                            setcity('')
+                            setmobile(undefined)
+                        }}
+                    />
+                </>
+            )
+        }
+        if (editing && index === editIndex) {
+            return (
+                <>
+                    <Button
+                        label='Update'
+                        onClick={() => {
+                            let cd = [...data1]
+                            cd[editIndex]["name"] = name
+                            cd[editIndex]["city"] = city
+                            cd[editIndex]["mobile"] = mobile
+                            setData(cd)
+                            setediting(false)
+                            setname('')
+                            setcity('')
+                            setmobile(undefined)
+                        }}
+                    />
+                    <Button
+                        label='Cancel'
+                        onClick={() => {
+                            let cd = [...data1]
+                            setData(cd)
+                            setediting(false)
+                            setname('')
+                            setcity('')
+                            setmobile(undefined)
+                        }}
+                    />
+                </>
+            )
+        }
         return (
             <>
+                <Button
+                    label='Edit'
+                    onClick={() => {
+                        seteditIndex(index)
+                        setname(obj.name)
+                        setcity(obj.city)
+                        setmobile(obj.mobile)
+                        setediting(true)
+                    }}
+                />
                 <Button
                     label='Delete'
                     onClick={() => {
                         let cd = [...data1]
-                        cd.splice(data1.findIndex(a => a === b), 1);
+                        cd.splice(index, 1);
                         setData(cd)
                     }}
                 />
             </>
         )
     }
-    const textEditor = (s) => {
-        return (
-            <>
-                <InputText
-                    type="text"
-                    value={s.value}
-                    onChange={(e) => s.editorCallback(e.target.value)}
-                />
-            </>
-        )
+    const body = (obj, s) => {
+        let index = data1.findIndex(a => a === obj)
+        if ((adding && index === data1.length - 1)||(editing && index === editIndex)) {
+            switch (s) {
+                case "name":
+                    return (
+                        <>
+                            <InputText
+                                type="text"
+                                value={name}
+                                onChange={(e) => setname(e.target.value)}
+                            />
+                        </>
+                    )
+                case "city":
+                    return (
+                        <>
+                            <Dropdown
+                                value={city}
+                                options={cities}
+                                onChange={(e) => setcity(e.value)}
+                            />
+                        </>
+                    )
+                case "mobile":
+                    return (
+                        <>
+                            <InputText
+                                type="text"
+                                value={mobile}
+                                onChange={(e) => setmobile(e.target.value)}
+                            />
+                        </>
+                    )
+                default:
+                    break
+            }
+        }
+        return obj[`${s}`]
     }
-    const edit = (e) => {
-        let edited = [...data1];
-        let { newData, index } = e;
+    const add = () => {
+        let newData = [...data1]
+        newData.push({
+            "name": '',
+            'city': '',
+            'mobile': undefined
+        })
+        setData(newData)
+        setadding(true)
+    }
 
-        edited[index] = newData;
 
-        setData(edited);
-    }
-    const addrow = {
-        id: ids,
-        name: "",
-        city: "",
-        mobile: null
-    }
     return (
         <div className='container'>
             <div
@@ -87,43 +180,31 @@ const Table3 = () => {
                 <Button
                     className="float-end p-button"
                     label='Add'
-                    onClick={() => {
-                        setData([...data1, addrow])
-                        setActiveRowIndex(data1.length - 1)
-                        setids(prev => prev + 1)
-                    }}
+                    onClick={add}
                 />
             </div>
             <DataTable
                 value={data1}
-                editMode="row"
-                dataKey="id"
-                editingRows={editingRows}
-                onRowEditChange={onRowEditChange}
-                onRowEditComplete={edit}
                 responsiveLayout="scroll"
             >
                 <Column
-                    field="name"
                     header="Name"
-                    editor={(e) => textEditor(e)}
+                    field='name'
+                    body={(obj) => body(obj, "name")}
                 />
                 <Column
-                    field="city"
                     header="City"
-                    editor={(e) => dropEditor(e)}
+                    field='city'
+                    body={(obj) => body(obj, "city")}
                 />
                 <Column
-                    field="mobile"
                     header="Mobile"
-                    editor={(e) => textEditor(e)}
-                />
-                <Column
-                    rowEditor
+                    field='mobile'
+                    body={(obj) => body(obj, "mobile")}
                 />
                 <Column
                     header='Actions'
-                    body={(e) => action(e)}
+                    body={(obj) => action(obj)}
                 />
             </DataTable>
         </div>
